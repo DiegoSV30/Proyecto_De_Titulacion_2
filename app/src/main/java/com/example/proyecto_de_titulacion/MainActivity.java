4cf12953d51b8f7adad5eb6b9dc982b36a5671bb
DiegoSV30 <diego.sandovalvallejos21@gmail.com>
2024-10-18 12:28:32 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/proyecto_de_titulacion/MainActivity.java
+++ app/src/main/java/com/example/proyecto_de_titulacion/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Aquí puedes implementar la navegación a la pantalla de detalle
+                
             }
         });
     }
